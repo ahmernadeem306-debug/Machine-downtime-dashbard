d7c21bcb526b60e10d1706707107ebccc9148e1c
ahmernadeem306-debug <ahmernadeem306@gmail.com>
2026-05-06 15:42:09 +0500

    Fix variable name in file uploader for consistency

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Machine Downtime % Analytics - By Ahmer Nadeem")
 upload_file=st.file_uploader("Upload Machine Log CSV file", type="csv")
 if upload_file:
 
-    df= pd.read_csv(uploaded_file)
+    df= pd.read_csv(upload_file)
     st.subheader("Raw Data")
     st.dataframe(df.head())
     stop_time=df[df['Status']=='Stop']['Duration'].sum()

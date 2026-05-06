449046e7a28819f4ad184d9693d656a24827e334
ahmernadeem306-debug <ahmernadeem306@gmail.com>
2026-05-06 15:37:08 +0500

    Fix variable name for uploaded file in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 st.set_page_config(page_title="Machine Downtime dashboard",layout="wide")
 st.title("Machine Downtime % Analytics - By Ahmer Nadeem")
 upload_file=st.file_uploader("Upload Machine Log CSV file", type="csv")
-if uploaded_file:
+if upload_file:
 
     df= pd.read_csv(uploaded_file)
     st.subheader("Raw Data")

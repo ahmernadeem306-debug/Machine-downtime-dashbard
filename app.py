2c0df47492b104952468a0e00f51f96147c417c9
ahmernadeem306-debug <ahmernadeem306@gmail.com>
2026-05-06 15:14:52 +0500

    Fix indentation and correct file upload function

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 st.set_page_config(page_title="Machine Downtime dashboard",layout="wide")
-st.title("Machine Downtime % Analytics - By Ahmer Nadeem)
+st.title("Machine Downtime % Analytics - By Ahmer Nadeem")
 upload_file=st.file_uupload("Upload Machinne Log CSV file", type="csv")
 if uploaded_file:
 
